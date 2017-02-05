85ce43f16ae91422411a921a858fff7d577d8cfa
Wujek <mjwojcik94@gmail.com>
2017-02-05 16:18:03 +0100

    na pure JS

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,34 @@
 'use strict';
 
-var paragraph1 = document.getElementsByTagName('p')[0];
-var paragraph2 = document.getElementsByTagName('p')[1];
 
 function ustaw_tlo() {
-   
-   paragraph2.style.backgroundColor = "yellow";
-   paragraph1.style.backgroundColor = "red";
-   
+   p1.style.backgroundColor = "red";
+   p2.style.backgroundColor = "blue";      
 }
+
+var input = document.createElement('input');
+input.type = "button";
+input.value = "Kliknij aby zmienic kolor";
+input.setAttribute("onclick", "ustaw_tlo()");
+
+var paragraph1 = document.createElement('p');
+var paragraph2 = document.createElement('p');
+paragraph1.innerHTML = "Paragraf 1";
+paragraph2.innerHTML = "Paragraf 2";
+
+document.body.appendChild(paragraph2);
+document.body.appendChild(paragraph1);
+document.body.appendChild(input);
+
+var p1 = document.getElementsByTagName('p')[0];
+var p2 = document.getElementsByTagName('p')[1];
+
+
+
+
+
+
+
+//var paragraph1 = document.getElementsByTagName('p')[0];
+//var paragraph2 = document.getElementsByTagName('p')[1];
+
