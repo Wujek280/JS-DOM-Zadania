1e136f796db9003cb85352f8656c8dea6f141c1b
Wujek <mjwojcik94@gmail.com>
2017-02-05 15:39:30 +0100

    color_changer

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var paragraph1 = document.getElementsByTagName('p')[0];
+var paragraph2 = document.getElementsByTagName('p')[1];
+
+function ustaw_tlo() {
+   
+   paragraph2.style.backgroundColor = "yellow";
+   paragraph1.style.backgroundColor = "red";
+   
+}
